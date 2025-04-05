fcd0ba78c866a26e1b6368ea435879101589e888
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-04-05 14:58:48 -0400

    Create HelloWorld & workflow

---
+++ HelloWorld.go
@@ -0,0 +1,11 @@
+/*
+Stevens Institute of Technology
+*/
+package main //package declaration: main
+
+import "fmt" //import statement: fmt, Println comes from this package
+
+func main() { //function declaration: main
+	fmt.Println("Hello World")
+	fmt.Println(t.Now())
+}
